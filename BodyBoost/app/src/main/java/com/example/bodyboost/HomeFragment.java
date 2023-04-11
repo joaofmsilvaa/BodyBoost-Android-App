712c27565ad9ab918b73c600f8051a8efaf0564b
João Silva <109978656+JOao7640@users.noreply.github.com>
2023-04-11 22:44:54 +0100

    Home Screen
    The progress bar is done but the percentage shown in the middle is still not connected to the progress in the progress bar.
    Created a horizontal scrollview for the cards ( for now it only has buttons )

--- BodyBoost/app/src/main/java/com/example/bodyboost/HomeFragment.java
+++ BodyBoost/app/src/main/java/com/example/bodyboost/HomeFragment.java
@@ -7,13 +7,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
+ * Use the {@link HomeFragment} factory method to
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
+
+
+    private int percentageValue;
+    private TextView percentage;
+    private ProgressBar percentageBar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,4 +34,12 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+    public void percentageSetter(View view){
+
+        percentageValue = percentageBar.getProgress();
+        String percentageString = Integer.toString(percentageValue);
+        percentage.setText(percentageString);
+    }
+
 }
