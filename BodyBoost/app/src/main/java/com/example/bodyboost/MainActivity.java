6cf3725f5411280ecdfcc1a0e58db4e53226c8d9
João Silva <109978656+JOao7640@users.noreply.github.com>
2023-04-25 14:32:26 +0100

    sign in and up activities
    Coded the structure of the log-in and register activies

--- BodyBoost/app/src/main/java/com/example/bodyboost/MainActivity.java
+++ BodyBoost/app/src/main/java/com/example/bodyboost/MainActivity.java
@@ -1,29 +1,69 @@
 package com.example.bodyboost;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+
+    String usernameString;
+    String passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        /*setContentView(R.layout.activity_main);
+
         NavHostFragment navHostFragment =
                 (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
         NavController navController = navHostFragment.getNavController();
         BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
-        NavigationUI.setupWithNavController(bottomNav, navController);
+        NavigationUI.setupWithNavController(bottomNav, navController);*/
+        setContentView(R.layout.fragment_login);
+    }
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull String name, @NonNull Context context, @NonNull AttributeSet attrs) {
+        username =  (EditText) findViewById(R.id.usernameInput);
+        password = (EditText) findViewById(R.id.passwordInput);
+
+        usernameString = username.getText().toString();
+        passwordString = password.getText().toString();
+        Log.i("username", usernameString);
+        Log.i("password", passwordString);
+
+
+        return super.onCreateView(name, context, attrs);
+    }
+
+    public void signUpMenu(View view) {
+        Intent intent = new Intent(this, registerActivity.class);
+        Log.i("Register activity", "Entering registerActivity");
+        startActivity(intent);
+        finish();
     }
 
 }
---
+++ BodyBoost/app/src/main/java/com/example/bodyboost/registerActivity.java
@@ -0,0 +1,33 @@
+package com.example.bodyboost;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class registerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*setContentView(R.layout.activity_main);
+
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+        NavigationUI.setupWithNavController(bottomNav, navController);*/
+        setContentView(R.layout.fragment_register);
+
+
+    }
+
+    public void signINMenu(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        Log.i("Main activity", "Entering the main activity");
+        startActivity(intent);
+        finish();
+    }
+}
