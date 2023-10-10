314f5bc4f77b01dc7b884f19b42a1eb9ecc35f56
João Silva <109978656+JOao7640@users.noreply.github.com>
2023-10-10 13:28:35 +0100

    Started creating meal_ingredients

---
+++ website/BodyBoost/database/migrations/2023_10_10_115846_create_meal_ingredients_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('meal_ingredients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreignId('ingredients_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('meal_id')->constrained()->cascadeOnDelete();
+            $table->float('amount');
+            $table->string('measure');
+            $table->integer('time')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recipes');
+    }
+};
