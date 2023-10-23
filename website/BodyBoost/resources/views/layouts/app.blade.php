c0f066ac08278532d9ea78a80d1942f9380c412d
João Silva <109978656+JOao7640@users.noreply.github.com>
2023-10-23 12:34:46 +0100

    Added the user profile
    Added the profile for the user to change his infos and delete his account

--- website/BodyBoost/resources/views/layouts/app.blade.php
+++ website/BodyBoost/resources/views/layouts/app.blade.php
@@ -13,14 +13,14 @@
 
 
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <body class="font-sans antialiased bg-gray-100">
+        <div class="min-h-screen dark:bg-gray-900">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <div class="max-w-7xl mx-auto pt-2 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
