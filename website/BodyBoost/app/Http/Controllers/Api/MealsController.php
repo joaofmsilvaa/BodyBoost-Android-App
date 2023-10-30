7b00e0080adce8c20bf46e0fa0d7a98877d3aaf3
João Silva <109978656+JOao7640@users.noreply.github.com>
2023-10-30 00:03:38 +0000

    Created an api endpoint to get all the meals
    Co-Authored-By: SaiNt._.LuKe <125977027+IM-SaiNt-LuKe@users.noreply.github.com>

---
+++ website/BodyBoost/app/Http/Controllers/Api/MealsController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\MealResource;
+use App\Models\Meal;
+use Illuminate\Http\Request;
+
+class MealsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $meals = Meal::all();
+        return MealResource::collection($meals);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Meal $meal)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Meal $meal)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Meal $meal)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Meal $meal)
+    {
+        //
+    }
+}
